f22106835d25916dfac5e7bec690c44daf17bffd
ns-spitsyna <spitcinan@gmail.com>
2019-02-05 22:52:18 +0700

    Урок 12.Домашнее задание

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from 'redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const reducer = (state = 0, action) =>{
+    switch(action.type){
+        case 'INC':
+            return state + 1;
+        case 'DEC':
+            return state - 1;
+        case 'RESET':
+            return state = 0; 
+        default:
+            return state;    
+    }
+    
+}
+
+const inc = () =>({ type: 'INC'});
+const dec = () =>({ type: 'DEC'});
+const reset = () =>({ type: 'RESET'});
+
+
+const store = createStore(reducer);
+
+document.getElementById('inc').addEventListener('click', () =>{
+    store.dispatch(inc())
+});
+document.getElementById('dec').addEventListener('click', () =>{
+    store.dispatch(dec())
+});
+document.getElementById('reset').addEventListener('click', () =>{
+    store.dispatch(reset())
+});
+
+
+const update = () =>{
+    document.getElementById('counter').textContent=store.getState();
+}
+store.subscribe(update);
+
+
+
+
