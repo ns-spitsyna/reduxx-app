3cbc8ae152280216730f1ff4965d5f11edbe0763
ns-spitsyna <spitcinan@gmail.com>
2019-02-06 16:43:02 +0700

    Урок 13. Домашнее задание

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
-//import { bindActionCreators} from 'redux';
+
 
 const Counter = ({counter, inc, dec, reset}) =>{
     return (
@@ -29,8 +29,5 @@ const mapStateToProps = (state) =>{
     }
 }
 
-// const mapDispatchToProps = (dispatch) => {
-// return bindActionCreators(actions, dispatch);
-   
-// }
+
 export default connect(mapStateToProps, actions)(Counter);
--- src/index.js
+++ src/index.js
@@ -3,24 +3,15 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reduсer';
-//import * as actions from './actions';
-//import Counter from './components/counter';
 import App from './components/app';
 
 const store = createStore(reducer);
-//const {dispatch} = store;
-
-
-
-// const update = () =>{
-   
+  
     ReactDOM.render(
     <Provider store = {store}>
         <App/>
     </Provider>, document.getElementById('root'));
-// }
-// update();
-// store.subscribe(update);
+
 
 
 
